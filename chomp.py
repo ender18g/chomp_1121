a9bf803d8d832c6d24ba67c5ee9543c74eafedb4
Allan Elsberry <37273149+ender18g@users.noreply.github.com>
2023-10-23 08:32:55 -0400

    added class fish

--- chomp.py
+++ chomp.py
@@ -2,16 +2,27 @@
 import pygame
 from helpers import *
 
-# this function will take 2 surface and center the 2nd surface on the first one
-def center_surfaces(bg, fg):
-    # get the bg width and height
-    bg_width = bg.get_width()
-    bg_height = bg.get_height()
-    # get the front surface width and height
-    fg_width = fg.get_width()
-    fg_height = fg.get_height()
-    # blit the text on the surface
-    bg.blit(fg, (bg_width/2 - fg_width/2, bg_height/2-fg_height/2 ))
+import random
+class Fish():
+    def __init__(self,screen):
+        self.image = pygame.image.load('assets/images/fishTile_079.png')
+        self.image = pygame.transform.flip(self.image, 1,0)
+        self.speed = random.randint(1,5)
+        self.x = screen.get_width()
+        self.y = random.randint(0,screen.get_height())
+        self.screen = screen
+
+    def update(self):
+        # move the fish to the left
+        self.x -=self.speed
+        if self.x<0:
+            self.x = self.screen.get_width()
+
+    def draw(self):
+        # blit the fish on the screen
+        self.screen.blit(self.image, (self.x,self.y))
+
+
 
 # pygame setup
 pygame.init()
@@ -30,9 +41,25 @@ print(pygame.font.get_fonts())
 # declare a Font
 game_font = pygame.font.SysFont('impact', 120)
 
+blue_fish_img = pygame.image.load('assets/images/fishTile_077.png')
+blue_fish_img = pygame.transform.flip(blue_fish_img,1,0)
+blue_fish_x = WIDTH
+
+fish1 = Fish(screen)
+
+
 
 running = True
 while running:
+    # move my fish to the left by 1 pixel
+    blue_fish_x -=3
+    fish1.update()
+
+    # make sure my fish is not off the left of the screen
+    if blue_fish_x <= 0-blue_fish_img.get_width():
+        blue_fish_x=WIDTH
+
+
     # poll for events
     # pygame.QUIT event means the user clicked X to close your window
     for event in pygame.event.get():
@@ -43,12 +70,14 @@ while running:
     screen.blit(background, (0, 0))
     # draw text
     font_surface = game_font.render('CHOMP',1,(199, 23, 4))
-
     center_surfaces(screen, font_surface)
 
+    # blit our blue fish
+    screen.blit(blue_fish_img, (blue_fish_x, HEIGHT/2))
+    fish1.draw()
 
+    clock.tick(60) # run at 60 FPS
 
-    clock.tick()
     pygame.display.set_caption(f"CHOMP {clock.get_fps():3}")
 
     # flip() the display to put your work on screen
--- helpers.py
+++ helpers.py
@@ -1,6 +1,17 @@
 import pygame
 from random import randint
 
+# this function will take 2 surface and center the 2nd surface on the first one
+def center_surfaces(bg, fg):
+    # get the bg width and height
+    bg_width = bg.get_width()
+    bg_height = bg.get_height()
+    # get the front surface width and height
+    fg_width = fg.get_width()
+    fg_height = fg.get_height()
+    # blit the text on the surface
+    bg.blit(fg, (bg_width/2 - fg_width/2, bg_height/2-fg_height/2 ))
+
 def make_background(screen):
     WIDTH = screen.get_width()
     HEIGHT = screen.get_height()
